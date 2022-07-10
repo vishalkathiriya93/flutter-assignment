d59c521673708729416e036257fded033c57a781
Asaf <asafgaber@gmail.com>
2022-07-10 17:07:04 +0300

    lib/providers/

---
+++ lib/providers/questions.dart
@@ -0,0 +1,25 @@
+class Question {}
+
+class Questions {
+  // [{
+  //   "incorrect": [
+  //     "Machine learning causes global warming",
+  //     "Unsecure networks lead to terrorist attacks",
+  //     "NFTs contribute to poaching of animals"
+  //   ],
+  //   "text": "What is one potential environmental concern about Web 3.0?",
+  //   "correct": "Blockchains can use a lot of energy"
+  // }, {
+  //   "incorrect": [
+  //     "Paying someone through PayPal",
+  //     "Buying something on Amazon.com",
+  //     "Taking a screenshot of an NFT"
+  //   ],
+  //   "text": "Which of the following is an example of a trustless transaction that takes place on Web 3.0?",
+  //   "correct": "Sending Bitcoin to someone else"
+  // }]
+  final List<Question> _items = [];
+  List<Question> get items {
+    return [..._items];
+  }
+}
